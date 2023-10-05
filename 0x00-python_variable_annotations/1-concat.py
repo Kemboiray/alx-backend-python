d7091f3c6b333e730cb86f69ff8b64fe89ebd4eb
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-05 22:52:44 +0300

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module defines a type-annotated function that takes strings `str1` `str2`
+as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return the concatenation of two strings
+    """
+    return str1 + str2
