883225a4bd468c5457566c5d4f3697e97c796083
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-06 03:16:07 +0300

    Add documentation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module defines a type-annotated function `zoom_array` that takes a tupl
+and returns a list.
+"""
 
 from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Return a list of tuples zoomed in `factor` times.
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
