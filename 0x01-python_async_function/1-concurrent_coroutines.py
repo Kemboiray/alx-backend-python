9bfa26d51ff11822cdd5fccd099880cb33ecb380
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-13 14:34:36 +0300

    Modify annotation to use `typing.List`

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,15 +2,17 @@
 """This module defines an async coroutine `wait_n`"""
 
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn `wait_random` `n` times with specified `max_delay`
 
     Returns: List of delays in ascending order
     """
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return sorted(list(delays))
+    return sorted(
+        list(await
+             asyncio.gather(*(wait_random(max_delay) for _ in range(n)))))
