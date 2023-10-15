b63f01b4c95ab820f739ae87474320a2fa6b0ba8
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-15 14:45:52 +0300

    Change return type annotation to `Generator[None, None]`

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ number between 0 and 10.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     This coroutine loops 10 times, each time asynchronously waiting 1
     second, then yields a random number between 0 and 10.
