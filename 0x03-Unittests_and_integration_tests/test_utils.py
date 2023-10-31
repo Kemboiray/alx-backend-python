d59affa0fa058c34b450b679535728dce00d2373
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-31 12:09:58 +0300

    Assert that `KeyError` is raised with the correct message

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,48 +6,40 @@ from unittest.main import main
 from unittest.mock import patch, MagicMock
 from utils import *
 from parameterized import parameterized
-from typing import Tuple, Dict, Any
+from typing import Tuple, Mapping, Dict, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Define a test for ``access_nested_map``"""
 
-    @parameterized.expand([({
-        "a": 1
-    }, ("a", ), 1), ({
-        "a": {
-            "b": 2
-        }
-    }, ("a", ), {
-        "b": 2
-    }), ({
-        "a": {
-            "b": 2
-        }
-    }, ("a", "b"), 2)])
-    def test_access_nested_map(self, nested_map: Dict, path: Tuple,
+    @parameterized.expand([
+        ({"a": 1}, ("a", ), 1),
+        ({"a": {"b": 2}}, ("a", ), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Tuple,
                                expected: Any):
-        """Test that the method returns what it is supposed to"""
+        """``access_nested_map`` returns the correct value given a path\n"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([({}, ("a", )), ({"a": 1}, ("a", "b"))])
-    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple):
-        """Test that a KeyError is raised"""
+    @parameterized.expand([({}, ("a", ), "a"), ({"a": 1}, ("a", "b"), "b")])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Tuple, msg: str):
+        """KeyError is raised and includes expected exception message\n"""
         with self.assertRaises(KeyError) as ke:
             access_nested_map(nested_map, path)
-            self.assertIn(ke.exception.args[0], path)
+            self.assertEqual(ke.exception.args[0], msg)
 
 
 class TestGetJson(unittest.TestCase):
     """This class encapsulates a test for the ``get_json`` function"""
 
-    @parameterized.expand([("http://example.com", {
-        "payload": True
-    }), ("http://holberton.io", {
-        "payload": False
-    })])
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
     def test_get_json(self, test_url: str, test_payload: Dict):
-        """Test ``get_json``"""
+        """``get_json`` returns the correct JSON response\n"""
         with patch("utils.requests.get") as mock_get:
             mock_response = MagicMock()
             mock_response.json.return_value = test_payload
